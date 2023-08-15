08f63d76be6f8fd3bc6439bf90a17ce611f6fb28
Dmitryser1 <dmitryserafimovich@email.com>
2023-08-15 16:22:34 +0400

    router 2.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
 
-import { RouterProvider } from 'react-router-dom';
-import router from './router/router'
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Profile from './pages/Profile'
+import Error from './pages/Error'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Navbar from './components/Navbar'
+import { useContext } from 'react';
+import { AuthContext } from './context/authContext'
+import { DarkModeContext } from './context/darkModeContext'
 
 function App() {
+
+  const { currentUser } = useContext(AuthContext);
+
+  const { darkMode } = useContext(DarkModeContext)
+
+  const Layout = () => {
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"} `}>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+
+        </div>
+      </div>
+    );
+  };
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        },
+        {
+          path: "/profile/cart/:cartid",
+          element: <Cart />
+        },
+        {
+          path: "*",
+          element: <Error />
+        }
+
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    }
+  ])
+
   return (
     <div className="App">
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
