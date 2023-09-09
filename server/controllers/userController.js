9533d238b23a140ebe898b4b15d366497c10b7ee
Dmitryser1 <dmitryserafimovich@email.com>
2023-09-09 15:56:22 +0400

    Server done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,9 +3,9 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { User, Cart } = require('../models/models')
 
-const generateJwt = (id,email,role) => {
+const generateJwt = (id, email, role) => {
     return jwt.sign(
-        { id, email, role},
+        { id, email, role },
         process.env.SECRET_KEY,
         { expiresIn: "24h" }
     )
@@ -14,7 +14,7 @@ class UserController {
     async registration(req, res, next) {
         const { email, password, role } = req.body
         if (!email || !password) {
-            return next(ApiError.badRequest('Email or passord is incorrect'))
+            return next(ApiError.badRequest('Email or password is incorrect'))
         }
         const usertaken = await User.findOne({ where: { email } })
         if (usertaken) {
@@ -23,18 +23,26 @@ class UserController {
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({ email, role, password: hashPassword })
         const cart = await Cart.create({ userId: user.id })
-        const token = generateJwt(user.id, user.email, user.role) 
-        return res.json({token})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({ token })
     }
-    async login(req, res) {
+    async login(req, res, next) {
+        const { email, password } = req.body
+        const user = await User.findOne({ where: { email } })
+        if (!user) {
+            return next(ApiError.badRequest('Incorrect email'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.badRequest('Incorrect password'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({ token })
 
     }
     async check(req, res, next) {
-        const { id } = req.query
-        if (!id) {
-            return next(ApiError.badRequest('NOID'))
-        }
-        res.json(id)
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
     async delete(req, res) {
     }
